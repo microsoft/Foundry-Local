036325b72b20c38e799771f24416ad2ec17e9bce
Scott McKay <Scott.McKay@microsoft.com>
2025-11-20 13:36:08 +1000

    Update to 0.8.2 SDK (#315)
    * Update to new Foundry SDK
    
    * Update SDK.
    Make exception handling nicer in EP registration
    
    * Tweak output

--- samples/cs/GettingStarted/src/Shared/Utils.cs
+++ samples/cs/GettingStarted/src/Shared/Utils.cs
@@ -1,3 +1,4 @@
+using Microsoft.AI.Foundry.Local;
 using Microsoft.Extensions.Logging;
 using System.Text;
 
@@ -22,13 +23,30 @@ internal static class Utils
         return _loggerFactory.CreateLogger("FoundryLocalSamples");
     }
 
-    internal static async Task RunWithSpinner<T>(string msg, T workTask) where T : Task
+    internal static async Task RunWithSpinner<T>(string msg, T workTask, bool warnOnException = true) where T : Task
     {
         // Start the spinner
         using var cts = new CancellationTokenSource();
         var spinnerTask = ShowSpinner(msg, cts.Token);
 
-        await workTask;     // wait for the real work to finish
+        try
+        {
+            await workTask;     // wait for the real work to finish
+        }
+        catch (Exception fex)
+        {
+            // we're only using this for EP registration currently an exception here is non-fatal as we have built-in
+            // execution providers that can be used. in a production app you may want to handle this differently.
+            if (warnOnException)
+            {
+                cts.Cancel();
+                Console.WriteLine($"\nWarning: {fex.Message}");
+                return;
+            }
+            
+            throw;  // rethrow otherwise
+        }
+        
         cts.Cancel();       // stop the spinner
         await spinnerTask;  // wait for spinner to exit
     }
@@ -43,12 +61,12 @@ internal static class Utils
 
         while (!token.IsCancellationRequested)
         {
-            Console.Write($"{msg}\t{sequence[counter % sequence.Length]}");
             Console.SetCursorPosition(0, Console.CursorTop);
+            Console.Write($"{msg}... {sequence[counter % sequence.Length]}\t");
             counter++;
             await Task.Delay(200, token).ContinueWith(_ => { });
         }
 
-        Console.WriteLine($"\nDone.\n");
+        Console.WriteLine($"Done.\n");
     }
 }
