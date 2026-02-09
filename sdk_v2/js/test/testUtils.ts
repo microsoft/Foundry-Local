991e72fc43ac92e9446af1884daf7df0ea947f84
Prathik Rao <prathikrao@microsoft.com>
2026-02-09 08:14:25 -0800

    upload logs

--- sdk_v2/js/test/testUtils.ts
+++ sdk_v2/js/test/testUtils.ts
@@ -33,7 +33,8 @@ export const IS_RUNNING_IN_CI = isRunningInCI();
 export const TEST_CONFIG: FoundryLocalConfig = {
     appName: 'FoundryLocalTest',
     modelCacheDir: getTestDataSharedPath(),
-    logLevel: 'warn'
+    logLevel: 'warn',
+    logsDir: path.join(getGitRepoRoot(), 'sdk_v2', 'js', 'logs')
 };
 
 export const TEST_MODEL_ALIAS = 'qwen2.5-0.5b';
