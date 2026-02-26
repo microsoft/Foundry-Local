7728604daac152c811576647086091bbecfdedde
kunal-vaishnavi <115581922+kunal-vaishnavi@users.noreply.github.com>
2026-02-26 13:42:01 -0800

    Add tool calling and structured outputs to FL SDK v2 (#456)
    * Add tool calling and structured outputs to FL C# SDK
    
    * Fix JSON parsing and parameter order for unit tests
    
    * Fix order of expected arguments
    
    * Get FL C# SDK E2E samples via interop and web working
    
    * Increase ORT GenAI version to 0.12.1
    
    * Get FL JS SDK E2E samples via interop and web working
    
    * Remove changes made for local testing purposes
    
    * Add changes suggested during PR review
    
    * Separate tool calling examples
    
    * Remove unneeded param attribute
    
    * Update FL Core versions
    
    * Omit passing in FL Core version in C# YAML file for now
    
    * Remove useNightly usage from JS build
    
    * Remove nightly flag from npm install
    
    * Put back useNightly usage in JS build

---
+++ sdk_v2/cs/test/FoundryLocal.Tests/OperatingSystemConverter.cs
@@ -0,0 +1,17 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright company="Microsoft">
+//   Copyright (c) Microsoft. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+public static class OperatingSystemConverter
+{
+    public static string ToJson(string s)
+    {
+        if (!OperatingSystem.IsWindows())
+        {
+            s = s.Replace("\r\n", "\n");
+        }
+        return s;
+    }
+}
