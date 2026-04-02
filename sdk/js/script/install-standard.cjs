50896e23e1ebde8c40d34fa6e5cb52444c2cca8a
rui-ren <ruiren1225@gmail.com>
2026-04-01 19:31:03 -0700

    win ml pkg

--- sdk/js/script/install-standard.cjs
+++ sdk/js/script/install-standard.cjs
@@ -12,7 +12,7 @@ const useNightly = process.env.npm_config_nightly === 'true';
 
 const ARTIFACTS = [
     { name: 'Microsoft.AI.Foundry.Local.Core', version: '0.9.0-dev-202603310538-f6efa8d3', feed: ORT_NIGHTLY_FEED, nightly: useNightly },
-    { name: os.platform() === 'linux' ? 'Microsoft.ML.OnnxRuntime.Gpu.Linux' : 'Microsoft.ML.OnnxRuntime.Foundry', version: '1.24.3', feed: NUGET_FEED, nightly: false },
+    { name: os.platform() === 'linux' ? 'Microsoft.ML.OnnxRuntime.Gpu.Linux' : 'Microsoft.ML.OnnxRuntime.Foundry', version: '1.23.2', feed: NUGET_FEED, nightly: false },
     { name: 'Microsoft.ML.OnnxRuntimeGenAI.Foundry', version: '0.13.0-dev-20260319-1131106-439ca0d5', feed: ORT_NIGHTLY_FEED, nightly: useNightly },
 ];
 
