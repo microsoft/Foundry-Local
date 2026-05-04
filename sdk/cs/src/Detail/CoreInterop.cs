0578b7cfc027ecff35af9d926fbfd78b678870b3
Prathik Rao <prathikrao@microsoft.com>
2026-05-04 09:17:09 -0700

    rev

--- sdk/cs/src/Detail/CoreInterop.cs
+++ sdk/cs/src/Detail/CoreInterop.cs
@@ -16,7 +16,7 @@ using static Microsoft.AI.Foundry.Local.Detail.ICoreInterop;
 internal partial class CoreInterop : ICoreInterop
 {
     // TODO: Android and iOS may need special handling. See ORT C# NativeMethods.shared.cs
-    internal const string LibraryName = "Microsoft.AI.Foundry.Local.Core.dll";
+    internal const string LibraryName = "Microsoft.AI.Foundry.Local.Core";
 
     private readonly ILogger _logger;
 
